8e649f7caca0a7e895e5515a2bda51b9ebc5f934
folder <ht51566@naver.com>
2022-10-10 21:07:54 +0900

    2022-10-10
    포인터 시작

---
+++ CPP_Study/CPP_Study/CPP_Study15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 오늘의 주제 : 포인터 
+
+int main()
+{
+	// 지금까지 사용한 방식
+	// number라는 이름의 4바이트 정수 타입의 바구니를 만든다
+	// number라는 변수 스택 메모리에 할당
+	// number = 1라 함은, number 바구니에 1이라는 숫자를 넣으라는 의미,
+	// 따라서 스택 메모리에 있는 특정 주소(number 바구니)에 우리가 원하는 값을 넣은 셈
+	// number는 비유하자면 메모리에 이름을 붙인 것 (찰떡같이 알아들어서)
+	// 나쁘지 않고 편리한데. 단점은 TextRPG 원본 수정
+	int number = 1;
+
+	return 0;
+}
